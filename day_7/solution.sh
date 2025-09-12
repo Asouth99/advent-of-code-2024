b60f51b433f8b6e214dc3fddb01274bc3b31aefe
Aadam Southon <a.southon@hotmail.com>
2025-09-12 16:55:45 +0100

    Uploaded part of day7 part 2. Added precalculated input

--- day_7/solution.sh
+++ day_7/solution.sh
@@ -111,45 +111,90 @@ solution_2 () {
     done < "$1"
     number_of_equations=$i
 
+    # Create output file
+    output_file="${1/.txt/}_output.txt"
+    if test -f $output_file ; then
+        echo "Output file exists! Exiting..."
+        exit 1
+    fi
+
+    # Read file with pre calculated inputs
+    echo "Reading Pre Calculated file: $2"
+    declare -a pre_calculated_results
+    declare -a pre_calculated_equations
+    i=0
+    while read -r line; do
+        IFS=":"
+        read -r result equation <<< "$line"
+        unset IFS
+        pre_calculated_results+=("$result")
+        pre_calculated_equations+=("$equation")
+        ((i++))
+    done < "$2"
+
+    # Get the equation with the most numbers
+    declare -a numbers_of_equation_with_most_numbers=()
+    IFS=" "
+    for ((i = 0 ; i < ${#equations[*]} ; i++ )); do
+        read -ra numbers <<< "${equations[$i]}"
+        if [[ ${#numbers[*]} -gt ${#numbers_of_equation_with_most_numbers[*]} ]]; then
+            numbers_of_equation_with_most_numbers=(${numbers[*]})
+        fi
+    done
+    unset IFS
+    echo "  Equation with the most numbers is ${numbers_of_equation_with_most_numbers[*]} with ${#numbers_of_equation_with_most_numbers[*]} numbers"
+
+    # Generate a list of all operations of operations
+    declare -a operations_options=()
+    allowed_operations=("+" "*" "|")
+    number_of_operations_to_try=$(( "${#allowed_operations[*]}" ** ("${#numbers_of_equation_with_most_numbers[*]}" - 1) ))
+    echo "    There will be ${number_of_operations_to_try} different combinations to calculate"
+
+    for ((j = 0 ; j < number_of_operations_to_try ; j++ )); do
+        operation=""
+        for ((k = 0 ; k < (${#numbers[*]} - 1) ; k++ )); do
+            operation_index=$(( (j / ( ${#allowed_operations[*]} ** k )) % ${#allowed_operations[*]} ))
+            operation="${allowed_operations[$operation_index]}${operation}"
+        done
+        # echo "Operation ${j}: ${operation}"
+        operations_options+=("$operation")
+    done
+    # echo "  Operation Options: ${operations_options[*]}"
+
 
     # Attempt to solve each equation
     IFS=" "
-    allowed_operations=("+" "*" "|")
-    for ((i = 6 ; i < "$number_of_equations" ; i++ )); do
-        [[ "$i" -gt "6" ]] && break
+    for ((i = 0 ; i < "$number_of_equations" ; i++ )); do
+        # [[ "$i" -gt "6" ]] && break
 
         # Set some vars
         result="${results[$i]}"
-        echo -n "  ($i/$number_of_equations) Attempting to solve | $result : "
+        count=$((i+1))
+        echo -n "  ($count/$number_of_equations) Attempting to solve | $result : "
         read -ra numbers <<< "${equations[$i]}"
         echo "${numbers[*]}"
 
-        # Create a list of all combinations of operations
-        declare -a operations_options=()
+        # Check if the number is in the pre calculated list. 
+        found_in_pre_calculated_list=false
+        for ((j = 0 ; j < "${#pre_calculated_results[*]}" ; j++ )); do
+            if [[ "$result" -eq "${pre_calculated_results[$j]}" ]]; then
+                echo "    Found $result in the pre calculated list. $result = ${pre_calculated_equations[$j]}"
+                found_in_pre_calculated_list=true
+                answer=$(( answer + result ))
+                break
+            fi
+        done
+        [[ "$found_in_pre_calculated_list" == "true" ]] && continue
+
+        # Find out how many different combinations we need to try
         number_of_operations_to_try=$(( "${#allowed_operations[*]}" ** ("${#numbers[*]}" - 1) ))
         echo "    There will be ${number_of_operations_to_try} different combinations to try"
-        for ((j = 0 ; j < ( "${number_of_operations_to_try}" ) ; j++ )); do
-            based_string=$(echo "obase=${#allowed_operations[*]};${j}" | bc)
-            based_string_length="${#based_string}"
-            # add 0 to the left to make sure based_string is the right length
-            for ((k = 0 ; k < ( "${#numbers[*]}" - 1 - "$based_string_length" ) ; k++ )); do
-                based_string="0${based_string}"
-            done
-            # echo -n "      Based String: $based_string"
-            operation=""
-            while read -rn 1 char; do
-                [[ ! "$char" =~ [0-9] ]] && continue
-                # echo "      Char: <$char> -> ${allowed_operations[$char]}"
-                operation="${operation}${allowed_operations[$char]}"
-            done <<< "$based_string "
-            # echo "      $operation"
-            operations_options+=("$operation")
-        done
-        # echo "      Operation Options: ${operations_options[*]}"
 
         # For each operation combination calcualte the value of it and check if it equals the result.
-        for ((j = 0 ; j < "${#operations_options[*]}" ; j++ )); do
+        for ((j = 0 ; j < number_of_operations_to_try ; j++ )); do
             operation="${operations_options[$j]}"
+            operation="${operation:(( ${#operation} - ${#numbers[*]} + 1  )):((${#numbers[*]} - 1))}"
+            # echo "      Trying ${operation}"
             string_to_calculate="${numbers[0]}"
             tmp_calculation="${numbers[0]}"
             for ((k = 1 ; k < "${#numbers[*]}" ; k++ )); do
@@ -173,7 +218,8 @@ solution_2 () {
             # echo "      Trying - $string_to_calculate = ${tmp_result}"
             if [[ "$tmp_result" -eq "$result" ]]; then
                 echo "      $string_to_calculate = $result | Success!"
-                answer=$((answer + $result))
+                echo "$result : $string_to_calculate"  >> "$output_file"
+                answer=$(( answer + result ))
                 break
             fi
         done
@@ -191,44 +237,6 @@ solution_2 () {
 # echo
 # solution_1 input.txt # 3312271365652 | Took 670 seconds
 
-# solution_2 example_input.txt
-# echo
-# solution_2 input.txt
-
-
-# Took 531 seconds just to calculate number 6
-
-seconds_start=$SECONDS
-
-for ((j = 0 ; j < 2048 ; j++ )); do
-    based_string=$(echo "obase=2;${j}" | bc)
-    based_string_length="${#based_string}"
-    # add 0 to the left to make sure based_string is the right length
-    for ((k = 0 ; k < ( 12 - 1 - "$based_string_length" ) ; k++ )); do
-        based_string="0${based_string}"
-    done
-    operation=""
-    while read -n 1 char; do
-        [[ ! "$char" =~ [0-9] ]] && continue
-        operation="${operation}${allowed_operations[$char]}"
-    done <<< "$based_string "
-done
-
-
-
-seconds_end=$SECONDS
-seconds_diff=$((seconds_end - seconds_start))
-echo "Answer took $seconds_diff seconds to calculate"
-
-
-seconds_start=$SECONDS
-operation_string_length="2"
-for ((j = 0 ; j < 2048 ; j++ )); do
-    echo
-done
-
-
-
-seconds_end=$SECONDS
-seconds_diff=$((seconds_end - seconds_start))
-echo "Answer took $seconds_diff seconds to calculate"
+solution_2 example_input.txt # example_input_pre_calculated.txt
+echo
+solution_2 input.txt input_pre_calculated.txt # 451970149727469 | Took 2901 seconds
