92fdf402a423661f754929b0db3cd34749c499ad
Aadam Southon <a.southon@hotmail.com>
2025-09-30 16:15:41 +0100

    Added day 10 part 2 solution

--- day_10/solution.sh
+++ day_10/solution.sh
@@ -80,7 +80,7 @@ traverse () {
 
     if [[ "${map_arr[$start_x,$start_y]}" = "9" ]]; then
         # Found the end of the trail
-        # echo "    Ended trail '${map_arr[$start_x,$start_y]}' at ($start_x,$start_y)"
+        echo "    Ended trail '${map_arr[$start_x,$start_y]}' at ($start_x,$start_y)"
         pathEndCoords["$start_x,$start_y"]="true"
         return
     fi
@@ -121,13 +121,105 @@ traverse () {
     fi
 }
 
+traverse2 () {
+    # echo "$rows $cols"
+    local start_x=$1
+    local start_y=$2
+
+    if [[ "${map_arr[$start_x,$start_y]}" = "9" ]]; then
+        # Found the end of the trail
+        # echo "    Ended trail '${map_arr[$start_x,$start_y]}' at ($start_x,$start_y)"
+        ((rating++))
+        return
+    fi
+
+    # Move left
+    new_x=$((start_x - 1))
+    new_y=$start_y
+    diff=$(( ${map_arr[$new_x,$new_y]} - ${map_arr[$start_x,$start_y]} ))
+    if [[ ( "$new_x" -ge "0" || "$new_x" -lt "$max_x" || "$new_y" -ge "0" || "$new_y" -lt "$max_y" ) && "$diff" = "1" ]]; then
+        # echo "    moved left"
+        traverse2 $new_x $new_y
+    fi
+    # Move right
+    new_x=$((start_x + 1))
+    new_y=$start_y
+    diff=$(( ${map_arr[$new_x,$new_y]} - ${map_arr[$start_x,$start_y]} ))
+    if [[ ( "$new_x" -ge "0" || "$new_x" -lt "$max_x" || "$new_y" -ge "0" || "$new_y" -lt "$max_y" ) && "$diff" = "1" ]]; then
+        # echo "    moved right"
+        traverse2 $new_x $new_y
+    fi
+    
+    # Move up
+    new_x=$start_x
+    new_y=$((start_y - 1))
+    diff=$(( ${map_arr[$new_x,$new_y]} - ${map_arr[$start_x,$start_y]} ))
+    if [[ ( "$new_x" -ge "0" || "$new_x" -lt "$max_x" || "$new_y" -ge "0" || "$new_y" -lt "$max_y" ) && "$diff" = "1" ]]; then
+        # echo "    moved up"
+        traverse2 $new_x $new_y
+    fi
+    
+    # Move down
+    new_x=$start_x
+    new_y=$((start_y + 1))
+    diff=$(( ${map_arr[$new_x,$new_y]} - ${map_arr[$start_x,$start_y]} ))
+    if [[ ( "$new_x" -ge "0" || "$new_x" -lt "$max_x" || "$new_y" -ge "0" || "$new_y" -lt "$max_y" ) && "$diff" = "1" ]]; then
+        # echo "    moved down"
+        traverse2 $new_x $new_y
+    fi
+}
+
 
 solution_2 () {
     seconds_start=$SECONDS
     echo "Reading file: $1"
     answer=0
     
+    # Trail starts at a 0 (trailhead)
+    # Trail must go from 0->1->2->3...->9
+    # Number of different routes to reach a 9 trailhead = rating
+    # Answer = sum of all trailhead ratings
 
+
+    # Get size of input map
+    rows=0
+    while read -r line; do
+        ((rows++))
+        cols=${#line}
+    done < "$1"
+    echo "$1 has $rows rows and $cols columns"
+
+    # Read input map into a key value array
+    declare -gA map_arr
+    r=0
+    while read -r line; do
+        for ((c = 0 ; c < "$cols" ; c++ )); do
+            map_arr["$c,$r"]="${line:$c:1}"
+        done
+        ((r++))
+    done < "$1"
+
+    # Print map after we have read into an array
+    echo "Input map looks like:"
+    print_map "$rows" "$cols" map_arr
+    
+    # Find all trailheads
+    max_x=$cols
+    max_y=$rows
+    for ((y = 0 ; y < "$max_y" ; y++ )); do
+        for ((x = 0 ; x < "$max_x" ; x++ )); do
+            if [[ "${map_arr[$x,$y]}" = "0" ]]; then
+                echo "Started trail '${map_arr[$x,$y]}' at ($x,$y)"
+                # Call Traverse - this will be a recursive function which will search through entire map until it finds a 9
+                echo "  Traversing..."
+                declare -g rating
+                rating=0
+                traverse2 $x $y
+                echo "  Rating is $rating"
+                ((answer+=rating))
+            fi
+        done
+    done
 
     echo "Answer is: $answer"
     seconds_end=$SECONDS
@@ -137,11 +229,11 @@ solution_2 () {
 
 
 # solution_1 example_input_simple.txt
-solution_1 example_input.txt
-echo
-solution_1 input.txt
-
-
-# solution_2 example_input.txt
+# solution_1 example_input.txt
 # echo
-# solution_2 input.txt 
+# solution_1 input.txt
+
+
+solution_2 example_input.txt
+echo
+solution_2 input.txt 
