28cea2ae8cb93dd300005d5d81a252b405383eb2
Aadam Southon <a.southon@hotmail.com>
2025-10-21 09:58:51 +0100

    Removed bash functions. Overhead of creating subshells makes it extremely slow

--- day_22/solution.sh
+++ day_22/solution.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-mix () {
-    secret=$1
-    mixed_n=$2
-    echo "$(( secret ^ mixed_n ))"
-}
-
-prune () {
-    secret=$1
-    echo "$(( secret % 16777216 ))"
-}
-
-
 solution_1 () {
     seconds_start=$SECONDS
     echo "Reading file: $1"
@@ -30,14 +18,14 @@ solution_1 () {
         echo "Starting with secret number $secret_n"
         for (( i = 0 ; i < "$GENERATIONS" ; i++ )); do
 
-            secret_n=$(mix "$secret_n" "$(( secret_n * 64 ))")
-            secret_n=$(prune "$secret_n")
+            secret_n=$(( secret_n ^ ( secret_n * 64 ) ))
+            secret_n=$(( secret_n % 16777216 ))
 
-            secret_n=$(mix "$secret_n" "$(( secret_n / 32 ))")
-            secret_n=$(prune "$secret_n")
+            secret_n=$(( secret_n ^ ( secret_n / 32 ) ))
+            secret_n=$(( secret_n % 16777216 ))
 
-            secret_n=$(mix "$secret_n" "$(( secret_n * 2048 ))")
-            secret_n=$(prune "$secret_n")
+            secret_n=$(( secret_n ^ ( secret_n * 2048 ) ))
+            secret_n=$(( secret_n % 16777216 ))
 
             # echo "  ($((i+1))/$GENERATIONS) secret number is $secret_n"
         done
@@ -57,6 +45,43 @@ solution_2 () {
     echo "Reading file: $1"
     answer=0
 
+    declare -a secret_numbers=()
+    while read -r line; do
+        secret_numbers+=("$line")
+    done < $1
+    # echo "secret_numbers: ${secret_numbers[*]}"
+
+    # declare -A all_secret_numbers=()  # [ initial_secret_num , generation_index ] = secret_num_at_generation_index
+    # declare -A all_prices=()          # [ initial_secret_num , generation_index ] = price_at_generation_index
+    # declare -A all_changes=()         # [ initial_secret_num , generation_index ] = change_at_generation_index
+
+    for num in "${secret_numbers[@]}"; do
+        secret_n=$num
+        echo "Starting with secret number $secret_n"
+        for (( i = 0 ; i < "$GENERATIONS" ; i++ )); do
+
+            # secret_n=$(mix "$secret_n" "$(( secret_n * 64 ))")
+            # secret_n=$(prune "$secret_n")
+            secret_n=$(( secret_n ^ ( secret_n * 64 ) ))
+            secret_n=$(( secret_n % 16777216 ))
+
+
+            # secret_n=$(mix "$secret_n" "$(( secret_n / 32 ))")
+            # secret_n=$(prune "$secret_n")
+            secret_n=$(( secret_n ^ ( secret_n / 32 ) ))
+            secret_n=$(( secret_n % 16777216 ))
+
+            # secret_n=$(mix "$secret_n" "$(( secret_n * 2048 ))")
+            # secret_n=$(prune "$secret_n")
+            secret_n=$(( secret_n ^ ( secret_n * 2048 ) ))
+            secret_n=$(( secret_n % 16777216 ))
+
+
+            # echo "  ($((i+1))/$GENERATIONS) secret number is $secret_n"
+        done
+        echo "After $GENERATIONS generations secret number = $secret_n"
+        answer=$((answer + secret_n))
+    done
     
 
     echo "Answer is: $answer"
@@ -64,13 +89,6 @@ solution_2 () {
     seconds_diff=$((seconds_end - seconds_start))
     echo "Answer took $seconds_diff seconds to calculate"
 }
-
-# Tests
-# echo "$(mix 42 15)" # 42 is secret number and we are mixing in 15. Should return 37
-# echo "$(prune 100000000)" # 100000000 is secret number. Should return 16113920
-
-
-
 
 # With secret number = 123 the next 10 numbers would be:
 # 15887950
@@ -86,7 +104,6 @@ solution_2 () {
 # declare -g GENERATIONS="10"
 # solution_1 example_input_2.txt
 
-
 # 2000th Secret number for each buyer. 1 (start) 8685429 (2000th)
 # 1:    8685429
 # 10:   4700978
@@ -95,11 +112,27 @@ solution_2 () {
 # declare -g GENERATIONS="2000"
 # solution_1 example_input.txt # Answer should be 37327623
 
-
-
 # declare -g GENERATIONS="2000"
 # solution_1 input.txt # 17612566393 took 15630 seconds
 
-# solution_2 example_input.txt
+
+
+# Starting with secret number 123
+# The prices = the units digit. Change from previous is the third column.
+#      123: 3 
+# 15887950: 0 (-3)
+# 16495136: 6 (6)
+#   527345: 5 (-1)
+#   704524: 4 (-1)
+#  1553684: 4 (0)
+# 12683156: 6 (2)
+# 11100544: 4 (-2)
+# 12249484: 4 (0)
+#  7753432: 2 (-2)
+# declare -g GENERATIONS="10"
+# solution_2 example_input_2.txt
+
+declare -g GENERATIONS="2000"
+solution_2 example_input.txt
 # echo
 # solution_2 input.txt
