6df44cbee947bb22cb8f85d756c34e9a9f6b9f47
Aadam Southon <a.southon@hotmail.com>
2025-10-24 12:50:06 +0100

    Day 9 completed

--- day_9/solution.sh
+++ day_9/solution.sh
@@ -92,20 +92,118 @@ solution_2 () {
     seconds_start=$SECONDS
     echo "Reading file: $1"
     answer=0
+    echo
     
-    # Test how much a var can store in bash
-    # a=""
-    # for ((i = 0 ; i < 30000 ; i++ )); do
-    #     a="${a}1"
-    # done
-    # echo "$a" > test.txt
+    # Read input
+    echo "step 0 - reading number"
+    declare -a input_number=()
+    while read -r -n 1 char; do
+        input_number+=("$char")
+    done < "$1"
+    for num in "${input_number[@]}"; do # Echo input number
+        echo -n "$num"
+    done
+    echo
 
-    # a=()
-    # for ((i = 0 ; i < 300000 ; i++ )); do
-    #     a+=("1")
-    # done
-    # echo "${a[@]}" > test2.txt
+    # Format number
+    declare -a formatted_number=()
+    file_count=0
+    echo
+    echo "step 1 - formatting number"
+    for ((i = 0 ; i < "${#input_number[@]}" ; i++ )); do
+        number="${input_number[$i]}"
+        if [[ $((i % 2)) = "0" ]]; then
+            char="$((i/2))"
+            file_count=$char
+        else
+            char="."
+        fi
+        for ((j = 0 ; j < number ; j++ )); do
+            formatted_number+=("$char")
+        done
+    done
+    for num in "${formatted_number[@]}"; do # Echo formatted number
+        echo -n "$num"
+    done
+    echo
+    echo "Highest file ID is $file_count in the formatted number"
 
+    # Rearrange number
+    declare -a rearraneged_number=("${formatted_number[@]}")
+    echo
+    echo "step 2 - rearrange number"
+    file_index_r=$(( ${#formatted_number[@]} - 1 ))
+    file_index_l=$(( ${#formatted_number[@]} - 1 ))
+    file_id="$file_count"
+    for ((i = file_id ; i >= 0 ; i-- )); do
+        file_id=$i
+        echo "  Rearranging file ID $file_id"
+
+        # Find the LEFT index of the file ID
+        while [[ "${formatted_number[$file_index_r]}" != "$file_id" ]]; do
+            ((file_index_r--))
+            if [[ "$file_index_r" -lt "0" ]]; then break; fi
+        done
+        # Find the LEFT index of the file ID
+        file_index_l=$file_index_r
+        while [[ "${formatted_number[$file_index_l]}" = "$file_id" ]]; do
+            ((file_index_l--))
+            if [[ "$file_index_l" -lt "0" ]]; then break; fi
+        done
+        file_size=$((file_index_r - file_index_l))
+        ((file_index_l++))
+        # echo "  Found file ID $file_id at L:$file_index_l, R:$file_index_r with size $file_size"
+
+        # Check if there is a space availble that fits file ID
+        space_available="false"
+        for ((j = 0 ; j < file_index_l ; j++ )); do
+            space_index_l=$j
+            char="${rearraneged_number[$j]}"
+            if [[ "$char" != "." ]]; then
+                continue
+            fi
+            space_index_r="$j"
+            while [[ "${rearraneged_number[$space_index_r]}" = "." ]]; do
+                ((space_index_r++))
+                if [[ "$space_index_r" -ge "${#rearraneged_number[@]}" ]]; then break 2; fi
+            done
+            space=$((space_index_r - space_index_l))
+            if [[ "$space" -ge "$file_size" ]]; then
+                space_available="true"
+                break
+            fi
+        done
+        ((space_index_r--))
+        if [[ "$space_available" = "false" ]]; then
+            # echo "    No space available to move file ID $file_id with size $file_size"
+            continue
+        fi
+        # echo "  Found space for file ID $file_id at L:$space_index_l, R:$space_index_r with size $file_size"
+
+        # There is space so make the swap
+        for ((j = 0 ; j < file_size ; j++ )); do
+            rearraneged_number["$(( j + space_index_l ))"]="$file_id"
+            rearraneged_number["$(( j + file_index_l ))"]="."
+        done
+
+    done
+    for num in "${rearraneged_number[@]}"; do # Echo rearranged number
+        echo -n "$num"
+    done
+    echo
+
+    # Calculate checksum
+    checksum="0"
+    echo
+    echo "step 3 - calculating checksum"
+    for ((i = 0 ; i < ${#rearraneged_number[@]} ; i++ )); do
+        char="${rearraneged_number[$i]}"
+        if [[ "$char" = "." ]]; then continue; fi
+        (( checksum+=(i * char) ))
+    done
+    echo "Checksum is $checksum"
+
+    echo
     echo "Answer is: $answer"
     seconds_end=$SECONDS
     seconds_diff=$((seconds_end - seconds_start))
@@ -117,9 +215,6 @@ solution_2 () {
 # echo
 # solution_1 input.txt # 6299243228569 took 10 seconds
 
-# solution_2 example_input.txt
+solution_2 example_input.txt # Should be 2858
 # echo
-# solution_2 input.txt 
-
-
-6299243228569
+solution_2 input.txt # 6326952672104 took 2311 seconds
